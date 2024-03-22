a9a31ea517e253d238b1b2fa27884e9c4d010dc8
Гришанов Клим <klim.grishanov@mail.ru>
2024-03-22 16:11:33 +0300

    B0: [+] core: add handlers

--- C&C/internal/handler/handler.go
+++ C&C/internal/handler/handler.go
@@ -1,19 +1,26 @@
 package handler
 
 import (
-	"candc/internal/usecase"
+	"candc/internal/service"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 type Handler struct {
-	services *usecase.UseCase
+	services *service.Service
+	logger   *zap.Logger
 }
 
-func NewHandler(services *usecase.UseCase) *Handler {
-	return &Handler{services: services}
+func NewHandler(services *service.Service, logger *zap.Logger) *Handler {
+	return &Handler{
+		services: services,
+		logger:   logger,
+	}
 }
 
-func InitRoute() *fiber.App {
+func InitRoute(handlers *Handler) *fiber.App {
 	router := fiber.New()
+	api := router.Group("/api")
+	api.Get("/instance/:id", handlers.GetInstance)
 	return router
 }
